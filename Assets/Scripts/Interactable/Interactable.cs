7573d61d1870476cca5101c03d01a4dab7822a43
Tyler Page <tyler@robotogames.com>
2022-10-01 17:44:46 -0700

    Made map smaller, fixed talkable objects

--- Assets/Scripts/Interactable/Interactable.cs
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IInteractable
+public abstract class IInteractable : MonoBehaviour
 {
     public void Interact();
 }
