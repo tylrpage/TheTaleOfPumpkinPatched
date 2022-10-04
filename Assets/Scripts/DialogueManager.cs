2421708c82d16d01363cf6dee51f2fe201ce98c3
Jacob L <jt_boygame@yahoo.com>
2022-10-04 18:05:06 -0400

    Added Typewriter effect to dialogue

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -9,12 +9,14 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TMP_Text dialogueText;
     [SerializeField] private float talkingCooldown;
+    [SerializeField] private float typingSpeed = 0.04f;
 
     // Used to tell other systems if a dialogue is currently being shown
     [NonSerialized] public bool IsCurrentlyTalking;
 
     // Used to have a cooldown between dialogues ending and new dialogues starting
     private bool _canStartDialogue;
+    private bool _canContinueDialogue;
     private int _pageIndex;
     private List<string> _textPages;
     private Action _completeCallback;
@@ -26,12 +28,12 @@ public class DialogueManager : MonoBehaviour
 
     private void Update()
     {
-        if (IsCurrentlyTalking && Input.GetButtonDown("Interact"))
+        if (IsCurrentlyTalking && Input.GetButtonDown("Interact") && _canContinueDialogue)
         {
             ShowNextPage();
         }
     }
-
+    
     public void StartShowDialogue(List<string> textPages, Action completeCallback = null)
     {
         if (!_canStartDialogue)
@@ -56,7 +58,7 @@ public class DialogueManager : MonoBehaviour
         if (_pageIndex < _textPages.Count)
         {
             string pageText = _textPages[_pageIndex];
-            dialogueText.text = pageText;
+            StartCoroutine(TypeDialogue(pageText));
         }
         else
         {
@@ -84,5 +86,21 @@ public class DialogueManager : MonoBehaviour
     {
         yield return null;
         IsCurrentlyTalking = true;
+    }
+    
+    private IEnumerator TypeDialogue(string dialogue)
+    {
+        dialogueText.text = "";
+        _canContinueDialogue = false;
+        foreach (char letter in dialogue.ToCharArray())
+        {
+            dialogueText.text += letter;
+
+            if (letter != ' ') 
+            {
+                yield return new WaitForSeconds(typingSpeed);
+            }
+        }
+        _canContinueDialogue = true;
     }
 }
