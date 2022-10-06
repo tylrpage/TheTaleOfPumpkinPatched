372c005f9a8d76167e7f6c1bae19793083fc2a6d
Tyler Page <tyler@robotogames.com>
2022-10-05 19:20:38 -0700

    Added rotation to flip up

--- Assets/Scripts/FlipWithBook.cs
+++ Assets/Scripts/FlipWithBook.cs
@@ -7,11 +7,17 @@ public class FlipWithBook : MonoBehaviour
 {
     private Vector3 _initialPosition;
     private Vector3 _offsetFromPagePivot;
+    private Quaternion _initialRotation;
+    private float _flipDownRotation = 80;
 
     private void Start()
     {
         _initialPosition = transform.position;
+        _initialRotation = transform.rotation;
         _offsetFromPagePivot = _initialPosition - GameManager.Instance.BookManager.LeftPage.position;
+
+        
+        StartCoroutine(FlipUp());
     }
 
     // Update is called once per frame
@@ -20,12 +26,29 @@ public class FlipWithBook : MonoBehaviour
         if (_initialPosition.x < GameManager.Instance.BookManager.RightPage.position.x)
         {
             // Rotate according to left page
-            transform.rotation =
-                Quaternion.LookRotation(transform.forward, -GameManager.Instance.BookManager.LeftPage.up);
-            Quaternion rot = Quaternion.Inverse(Quaternion.AngleAxis(180, Vector3.back));
-
-            transform.position = GameManager.Instance.BookManager.LeftPage.position + rot *
+            Quaternion rot = Quaternion.LookRotation(_initialRotation * Vector3.forward, -GameManager.Instance.BookManager.LeftPage.up);
+            Quaternion flipDown = Quaternion.AngleAxis(_flipDownRotation, Vector3.right);
+            transform.rotation = rot * flipDown;
+            //transform.rotation *= Quaternion.AngleAxis(90, transform.right);
+            
+            Quaternion adjustment = Quaternion.Inverse(Quaternion.AngleAxis(180, Vector3.back));
+            transform.position = GameManager.Instance.BookManager.LeftPage.position + adjustment *
                 (Quaternion.Inverse(GameManager.Instance.BookManager.LeftPage.localRotation) * _offsetFromPagePivot);
+        }
+        else
+        {
+            transform.rotation = _initialRotation * Quaternion.AngleAxis(_flipDownRotation, Vector3.right);
+        }
+    }
+
+    private IEnumerator FlipUp()
+    {
+        yield return new WaitForSeconds(3);
+
+        for (float t = 0; t < 2; t += Time.deltaTime)
+        {
+            _flipDownRotation = Mathf.Lerp(80, 0, t);
+            yield return null;
         }
     }
 }
