b869ff23a9fa532fe2540a2e408ea11f1d163d07
Tyler Page <tyler@robotogames.com>
2022-10-08 14:35:58 -0700

    Added lanterns, they turn on when completing tasks

--- Assets/Scripts/Lightable.cs
+++ Assets/Scripts/Lightable.cs
@@ -8,6 +8,8 @@ public class Lightable : MonoBehaviour
     [SerializeField] private Interactable trigger;
     [SerializeField] private Renderer lightRenderer;
     [SerializeField] private float fadeInTime;
+    [SerializeField] private Light light;
+    [SerializeField] private float finalIntensity;
 
     private void Awake()
     {
@@ -15,6 +17,10 @@ public class Lightable : MonoBehaviour
         
         var material = lightRenderer.material;
         material.color = new Color(material.color.r, material.color.g, material.color.b, 0);
+        if (light != null)
+        {
+            light.intensity = 0;
+        }
     }
 
     private void OnDestroy()
@@ -34,6 +40,10 @@ public class Lightable : MonoBehaviour
             float iNorm = i / fadeInTime;
             var material = lightRenderer.material;
             material.color = new Color(material.color.r, material.color.g, material.color.b, iNorm);
+            if (light != null)
+            {
+                light.intensity = Mathf.Lerp(0, finalIntensity, iNorm);
+            }
             yield return null;
         }
     }
