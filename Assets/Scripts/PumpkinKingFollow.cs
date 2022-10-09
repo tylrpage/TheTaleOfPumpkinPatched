c884bb9ecaf07a7811a9ba9a6c16bc2714be242e
Tyler Page <tyler@robotogames.com>
2022-10-08 18:03:24 -0700

    Added visual for pumpkin king pie

---
+++ Assets/Scripts/PumpkinKingFollow.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinKingFollow : MonoBehaviour
+{
+    [SerializeField] private Interactable startTrigger;
+    [SerializeField] private Interactable endTrigger;
+    [SerializeField] private Transform endPosition;
+    [SerializeField] private Vector3 playerOffset;
+    [SerializeField] private Transform player;
+    [SerializeField] private float travelToPlayerTime;
+    [SerializeField] private Transform kingTransform;
+    [SerializeField] private GameObject kingVisual;
+
+    private bool _isFollowing;
+    private Vector3 _initialPosition;
+
+    private void Awake()
+    {
+        startTrigger.Completed += StartFollow;
+        endTrigger.Completed += EndFollow;
+        _initialPosition = kingTransform.position;
+    }
+
+    private void Start()
+    {
+        kingVisual.SetActive(false);
+    }
+
+    private void LateUpdate()
+    {
+        if (_isFollowing)
+        {
+            kingTransform.position = player.position + playerOffset;
+        }
+    }
+
+    private void EndFollow()
+    {
+        // Only do it once
+        endTrigger.Completed -= EndFollow;
+        StartCoroutine(HopOffPlayer());
+    }
+
+    private void StartFollow()
+    {
+        kingVisual.SetActive(true);
+        StartCoroutine(HopOnPlayer());
+    }
+
+    private IEnumerator HopOnPlayer()
+    {
+        for (float i = 0; i < travelToPlayerTime; i += Time.deltaTime)
+        {
+            float iNorm = i / travelToPlayerTime;
+            kingTransform.position = Vector3.Lerp(_initialPosition, player.position + playerOffset, iNorm);
+            yield return null;
+        }
+        _isFollowing = true;
+    }
+
+    private IEnumerator HopOffPlayer()
+    {
+        _isFollowing = false;
+        Vector3 playersOriginalPosition = player.position + playerOffset;
+        for (float i = 0; i < travelToPlayerTime; i += Time.deltaTime)
+        {
+            float iNorm = i / travelToPlayerTime;
+            kingTransform.position = Vector3.Lerp(playersOriginalPosition, endPosition.position, iNorm);
+            yield return null;
+        }
+        kingVisual.SetActive(false);
+    }
+}
